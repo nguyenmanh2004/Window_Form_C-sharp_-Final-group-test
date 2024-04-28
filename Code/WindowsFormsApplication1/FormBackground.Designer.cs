83d48cfed4e38712d2c5bd5deda0d65ea9c51bf4
nguyễn mạnh <manharia123@gmail.com>
2024-04-28 12:12:27 +0700

    tạo form login

--- Code/WindowsFormsApplication1/FormBackground.Designer.cs
+++ Code/WindowsFormsApplication1/FormBackground.Designer.cs
@@ -50,6 +50,7 @@
             this.button1.Size = new System.Drawing.Size(177, 45);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormBackground
             // 
--- Code/WindowsFormsApplication1/FormBackground.cs
+++ Code/WindowsFormsApplication1/FormBackground.cs
@@ -16,5 +16,11 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormLogin frlogin = new FormLogin();
+            frlogin.ShowDialog();
+        }
     }
 }
