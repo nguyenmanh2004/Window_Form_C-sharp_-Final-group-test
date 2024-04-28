a31cbddb73e324f7a29a184a3129f2c54cebae54
nguyễn mạnh <manharia123@gmail.com>
2024-04-28 12:12:07 +0700

    tạo form connect

---
+++ Code/WindowsFormsApplication1/Connect database.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Connect_database
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtserver = new System.Windows.Forms.TextBox();
+            this.txtdatabase = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(181, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kết nối với cơ sở dữ liệu ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên máy chủ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên database";
+            // 
+            // txtserver
+            // 
+            this.txtserver.Location = new System.Drawing.Point(246, 78);
+            this.txtserver.Name = "txtserver";
+            this.txtserver.Size = new System.Drawing.Size(100, 20);
+            this.txtserver.TabIndex = 3;
+            // 
+            // txtdatabase
+            // 
+            this.txtdatabase.Location = new System.Drawing.Point(246, 130);
+            this.txtdatabase.Name = "txtdatabase";
+            this.txtdatabase.Size = new System.Drawing.Size(100, 20);
+            this.txtdatabase.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "tài khoản ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "mật khẩu";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(246, 186);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(100, 20);
+            this.txtuser.TabIndex = 7;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(246, 241);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(100, 20);
+            this.txtpass.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Connect_database
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 367);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtdatabase);
+            this.Controls.Add(this.txtserver);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Connect_database";
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Connect_database_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtserver;
+        private System.Windows.Forms.TextBox txtdatabase;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Code/WindowsFormsApplication1/Connect database.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.Odbc;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Connect_database : Form
+    {
+        public string Serv;
+        public string Dat;
+        public Connect_database()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_database_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ket noi sql
+                string connectionstring;
+                SqlConnection cnn = new SqlConnection();
+                connectionstring = "Data Source=" + txtserver.Text;
+                connectionstring += ";Initial Catalog=" + txtdatabase.Text;
+                connectionstring += ";User Id=" + txtuser.Text;
+                connectionstring += ";Password=" + txtpass.Text;
+               
+                cnn.ConnectionString = connectionstring;
+                cnn.Open();
+                MessageBox.Show("ket noi thanh cong"); cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("loi ke noi" + ex.Message);
+            }
+        }
+    }
+}
